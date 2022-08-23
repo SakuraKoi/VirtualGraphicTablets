a84c7b3264d4e73b2198fff394244202c122d95c
SakuraKooi <root@sakurakooi.cyou>
2022-08-23 22:50:42 +0800

    [coding] correct component name

--- VirtualGraphicTablets-Server/core/src/main/java/sakura/kooi/VirtualGraphicTablets/server/core/VTabletServer.java
+++ VirtualGraphicTablets-Server/core/src/main/java/sakura/kooi/VirtualGraphicTablets/server/core/VTabletServer.java
@@ -24,40 +24,47 @@ public class VTabletServer extends JFrame {
         initComponents();
         setPreferredSize(new Dimension(875, 580));
         setMinimumSize(new Dimension(720, 470));
+        numCanvaPosX.setEditor(new JSpinner.NumberEditor(numCanvaPosX, "#"));
+        numCanvaPosY.setEditor(new JSpinner.NumberEditor(numCanvaPosY, "#"));
+        numCanvaWidth.setEditor(new JSpinner.NumberEditor(numCanvaWidth, "#"));
+        numCanvaHeight.setEditor(new JSpinner.NumberEditor(numCanvaHeight, "#"));
     }
 
     private void initComponents() {
         // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
         panel1 = new JPanel();
         panel4 = new JPanel();
-        button1 = new JButton();
-        button3 = new JButton();
-        button2 = new JButton();
-        button4 = new JButton();
+        btnConnectUpstream = new JButton();
+        btnDisconnectUpstream = new JButton();
+        btnStartServer = new JButton();
+        btnStopServer = new JButton();
         label11 = new JLabel();
         panel5 = new JPanel();
-        spinner2 = new JSpinner();
-        spinner1 = new JSpinner();
-        spinner3 = new JSpinner();
-        spinner4 = new JSpinner();
-        button5 = new JButton();
+        label13 = new JLabel();
+        label14 = new JLabel();
+        numCanvaPosX = new JSpinner();
+        numCanvaPosY = new JSpinner();
+        label15 = new JLabel();
+        label16 = new JLabel();
+        numCanvaWidth = new JSpinner();
+        numCanvaHeight = new JSpinner();
         var vSpacer2 = new Spacer();
         panel6 = new JPanel();
-        label12 = new JLabel();
+        canvas = new JLabel();
         panel2 = new JPanel();
         label1 = new JLabel();
-        label2 = new JLabel();
+        lblUpstreamConnectStatus = new JLabel();
         label3 = new JLabel();
-        label10 = new JLabel();
+        lblUpstreamVersion = new JLabel();
         scrollPane1 = new JScrollPane();
-        coloredTextPane1 = new ColoredTextPane();
+        txtLogs = new ColoredTextPane();
         panel3 = new JPanel();
         label4 = new JLabel();
-        label5 = new JLabel();
+        lblServerRunningStatus = new JLabel();
         label6 = new JLabel();
-        label7 = new JLabel();
+        lblServerListenPort = new JLabel();
         label8 = new JLabel();
-        label9 = new JLabel();
+        lblServerConnectStatus = new JLabel();
 
         //======== this ========
         setTitle("VirtualGraphicTablet Server");
@@ -76,33 +83,33 @@ public class VTabletServer extends JFrame {
                     new EmptyBorder(5, 5, 5, 5)));
                 panel4.setLayout(new GridLayoutManager(7, 1, new Insets(0, 0, 0, 0), -1, -1));
 
-                //---- button1 ----
-                button1.setText("\u8fde\u63a5 VirtualTablet Server");
-                panel4.add(button1, new GridConstraints(0, 0, 1, 1,
+                //---- btnConnectUpstream ----
+                btnConnectUpstream.setText("\u8fde\u63a5 VirtualTablet Server");
+                panel4.add(btnConnectUpstream, new GridConstraints(0, 0, 1, 1,
                     GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH,
                     GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
                     GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
                     null, null, null));
 
-                //---- button3 ----
-                button3.setText("\u65ad\u5f00 VirtualTablet Server");
-                panel4.add(button3, new GridConstraints(1, 0, 1, 1,
+                //---- btnDisconnectUpstream ----
+                btnDisconnectUpstream.setText("\u65ad\u5f00 VirtualTablet Server");
+                panel4.add(btnDisconnectUpstream, new GridConstraints(1, 0, 1, 1,
                     GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH,
                     GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
                     GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
                     null, null, null));
 
-                //---- button2 ----
-                button2.setText("\u542f\u52a8 Graphic Server");
-                panel4.add(button2, new GridConstraints(2, 0, 1, 1,
+                //---- btnStartServer ----
+                btnStartServer.setText("\u542f\u52a8 Graphic Server");
+                panel4.add(btnStartServer, new GridConstraints(2, 0, 1, 1,
                     GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH,
                     GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
                     GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
                     null, null, null));
 
-                //---- button4 ----
-                button4.setText("\u505c\u6b62 Graphic Server");
-                panel4.add(button4, new GridConstraints(3, 0, 1, 1,
+                //---- btnStopServer ----
+                btnStopServer.setText("\u505c\u6b62 Graphic Server");
+                panel4.add(btnStopServer, new GridConstraints(3, 0, 1, 1,
                     GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH,
                     GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
                     GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
@@ -118,31 +125,55 @@ public class VTabletServer extends JFrame {
 
                 //======== panel5 ========
                 {
-                    panel5.setLayout(new GridLayoutManager(3, 2, new Insets(0, 0, 0, 0), -1, -1));
-                    panel5.add(spinner2, new GridConstraints(0, 0, 1, 1,
-                        GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH,
-                        GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
-                        GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
-                        null, null, null));
-                    panel5.add(spinner1, new GridConstraints(0, 1, 1, 1,
-                        GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH,
-                        GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
-                        GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
-                        null, null, null));
-                    panel5.add(spinner3, new GridConstraints(1, 0, 1, 1,
-                        GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH,
-                        GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
-                        GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
-                        null, null, null));
-                    panel5.add(spinner4, new GridConstraints(1, 1, 1, 1,
+                    panel5.setLayout(new GridLayoutManager(4, 2, new Insets(0, 0, 0, 0), -1, -1));
+
+                    //---- label13 ----
+                    label13.setText("Pos X");
+                    panel5.add(label13, new GridConstraints(0, 0, 1, 1,
                         GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH,
                         GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
                         GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
                         null, null, null));
 
-                    //---- button5 ----
-                    button5.setText("\u8bbe\u7f6e\u7ed8\u5236\u533a\u5c3a\u5bf8");
-                    panel5.add(button5, new GridConstraints(2, 0, 1, 2,
+                    //---- label14 ----
+                    label14.setText("Pos Y");
+                    panel5.add(label14, new GridConstraints(0, 1, 1, 1,
+                        GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH,
+                        GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
+                        GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
+                        null, null, null));
+                    panel5.add(numCanvaPosX, new GridConstraints(1, 0, 1, 1,
+                        GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH,
+                        GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
+                        GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
+                        null, null, null));
+                    panel5.add(numCanvaPosY, new GridConstraints(1, 1, 1, 1,
+                        GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH,
+                        GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
+                        GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
+                        null, null, null));
+
+                    //---- label15 ----
+                    label15.setText("Width");
+                    panel5.add(label15, new GridConstraints(2, 0, 1, 1,
+                        GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH,
+                        GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
+                        GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
+                        null, null, null));
+
+                    //---- label16 ----
+                    label16.setText("Height");
+                    panel5.add(label16, new GridConstraints(2, 1, 1, 1,
+                        GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH,
+                        GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
+                        GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
+                        null, null, null));
+                    panel5.add(numCanvaWidth, new GridConstraints(3, 0, 1, 1,
+                        GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH,
+                        GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
+                        GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
+                        null, null, null));
+                    panel5.add(numCanvaHeight, new GridConstraints(3, 1, 1, 1,
                         GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH,
                         GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
                         GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
@@ -169,7 +200,7 @@ public class VTabletServer extends JFrame {
             {
                 panel6.setBorder(new EtchedBorder());
                 panel6.setLayout(new GridLayoutManager(1, 1, new Insets(0, 0, 0, 0), -1, -1));
-                panel6.add(label12, new GridConstraints(0, 0, 1, 1,
+                panel6.add(canvas, new GridConstraints(0, 0, 1, 1,
                     GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH,
                     GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
                     GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
@@ -196,9 +227,9 @@ public class VTabletServer extends JFrame {
                     GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
                     null, null, null));
 
-                //---- label2 ----
-                label2.setText("\u672a\u8fde\u63a5");
-                panel2.add(label2, new GridConstraints(0, 1, 1, 1,
+                //---- lblUpstreamConnectStatus ----
+                lblUpstreamConnectStatus.setText("\u672a\u8fde\u63a5");
+                panel2.add(lblUpstreamConnectStatus, new GridConstraints(0, 1, 1, 1,
                     GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH,
                     GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW | GridConstraints.SIZEPOLICY_WANT_GROW,
                     GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
@@ -212,9 +243,9 @@ public class VTabletServer extends JFrame {
                     GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
                     null, null, null));
 
-                //---- label10 ----
-                label10.setText("114514");
-                panel2.add(label10, new GridConstraints(1, 1, 1, 1,
+                //---- lblUpstreamVersion ----
+                lblUpstreamVersion.setText("114514");
+                panel2.add(lblUpstreamVersion, new GridConstraints(1, 1, 1, 1,
                     GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH,
                     GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW | GridConstraints.SIZEPOLICY_WANT_GROW,
                     GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
@@ -228,7 +259,7 @@ public class VTabletServer extends JFrame {
 
             //======== scrollPane1 ========
             {
-                scrollPane1.setViewportView(coloredTextPane1);
+                scrollPane1.setViewportView(txtLogs);
             }
             panel1.add(scrollPane1, new GridConstraints(1, 1, 2, 1,
                 GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH,
@@ -251,9 +282,9 @@ public class VTabletServer extends JFrame {
                     GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
                     null, null, null));
 
-                //---- label5 ----
-                label5.setText("\u672a\u542f\u52a8");
-                panel3.add(label5, new GridConstraints(0, 1, 1, 1,
+                //---- lblServerRunningStatus ----
+                lblServerRunningStatus.setText("\u672a\u542f\u52a8");
+                panel3.add(lblServerRunningStatus, new GridConstraints(0, 1, 1, 1,
                     GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH,
                     GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW | GridConstraints.SIZEPOLICY_WANT_GROW,
                     GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
@@ -267,9 +298,9 @@ public class VTabletServer extends JFrame {
                     GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
                     null, null, null));
 
-                //---- label7 ----
-                label7.setText("23372");
-                panel3.add(label7, new GridConstraints(1, 1, 1, 1,
+                //---- lblServerListenPort ----
+                lblServerListenPort.setText("23372");
+                panel3.add(lblServerListenPort, new GridConstraints(1, 1, 1, 1,
                     GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH,
                     GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW | GridConstraints.SIZEPOLICY_WANT_GROW,
                     GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
@@ -283,9 +314,9 @@ public class VTabletServer extends JFrame {
                     GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
                     null, null, null));
 
-                //---- label9 ----
-                label9.setText("\u672a\u8fde\u63a5");
-                panel3.add(label9, new GridConstraints(2, 1, 1, 1,
+                //---- lblServerConnectStatus ----
+                lblServerConnectStatus.setText("\u672a\u8fde\u63a5");
+                panel3.add(lblServerConnectStatus, new GridConstraints(2, 1, 1, 1,
                     GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH,
                     GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW | GridConstraints.SIZEPOLICY_WANT_GROW,
                     GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
@@ -306,32 +337,35 @@ public class VTabletServer extends JFrame {
     // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
     private JPanel panel1;
     private JPanel panel4;
-    private JButton button1;
-    private JButton button3;
-    private JButton button2;
-    private JButton button4;
+    private JButton btnConnectUpstream;
+    private JButton btnDisconnectUpstream;
+    private JButton btnStartServer;
+    private JButton btnStopServer;
     private JLabel label11;
     private JPanel panel5;
-    private JSpinner spinner2;
-    private JSpinner spinner1;
-    private JSpinner spinner3;
-    private JSpinner spinner4;
-    private JButton button5;
+    private JLabel label13;
+    private JLabel label14;
+    private JSpinner numCanvaPosX;
+    private JSpinner numCanvaPosY;
+    private JLabel label15;
+    private JLabel label16;
+    private JSpinner numCanvaWidth;
+    private JSpinner numCanvaHeight;
     private JPanel panel6;
-    private JLabel label12;
+    private JLabel canvas;
     private JPanel panel2;
     private JLabel label1;
-    private JLabel label2;
+    private JLabel lblUpstreamConnectStatus;
     private JLabel label3;
-    private JLabel label10;
+    private JLabel lblUpstreamVersion;
     private JScrollPane scrollPane1;
-    private ColoredTextPane coloredTextPane1;
+    private ColoredTextPane txtLogs;
     private JPanel panel3;
     private JLabel label4;
-    private JLabel label5;
+    private JLabel lblServerRunningStatus;
     private JLabel label6;
-    private JLabel label7;
+    private JLabel lblServerListenPort;
     private JLabel label8;
-    private JLabel label9;
+    private JLabel lblServerConnectStatus;
     // JFormDesigner - End of variables declaration  //GEN-END:variables
 }
