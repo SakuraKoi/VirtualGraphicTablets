ff3d4b8478a88c48ccac1fcf9f5fd45c36183e02
sakurawa <root@sakurakooi.cyou>
2022-08-23 15:57:47 +0800

    [coding] implement classpath check

--- VirtualGraphicTablets-Server/bootstrap/src/main/java/sakura/kooi/VirtualGraphicTablets/server/bootstrap/Bootstrap.java
+++ VirtualGraphicTablets-Server/bootstrap/src/main/java/sakura/kooi/VirtualGraphicTablets/server/bootstrap/Bootstrap.java
@@ -2,9 +2,24 @@ package sakura.kooi.VirtualGraphicTablets.server.bootstrap;
 
 import lombok.extern.slf4j.Slf4j;
 
+import javax.swing.*;
+import java.io.File;
+
 @Slf4j
 public class Bootstrap {
     public static void main(String[] args) {
+        log.info("Checking classpath...");
+        File libsDir = new File("libs");
+        File coreJar = new File(libsDir, "vgt-core.jar");
+        if (!coreJar.exists()) {
+            JOptionPane.showMessageDialog(null, "VirtualGraphicTablets Core not found\nPath: ./libs/vgt-core.jar", "Launch failed", JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+        File protocolJar = new File(libsDir, "classes-dex2jar.jar");
+        if (!protocolJar.exists()) {
+            JOptionPane.showMessageDialog(null, "VirtualTablet thrift protocol not found\nPath: ./libs/classes-dex2jar.jar", "Launch failed", JOptionPane.ERROR_MESSAGE);
+            return;
+        }
 
         // TODO create a hybrid class loader
         // TODO first lookup core then VirtualTablets apk
