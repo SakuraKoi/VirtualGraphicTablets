e305b1463dc7fb09f7456dadf416b280ce3351e1
SakuraKooi <root@sakurakooi.cyou>
2022-08-23 19:17:22 +0800

    [refactor] use enjarify

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 # VirtualGraphicTablets
 把Android手机当数位屏用的实验性项目
 
-这玩意只是加了个图传, 数位板功能需要VirtualTablet Server
+这玩意只是加了个图传, 数位板功能通过VirtualTablet Server实现
 
-为避免dmca, VirtualTablet Server的thrift协议需要自行把VirtualTablet Lite版的apk跑dex2jar之后扔进classpath
+### How 2 use
+
+1. 下载release
+
+2. 为避免dmca, 需自行获取VirtualTablet Server的thrift协议
+
+   (把VirtualTablet的apk跑dex2jar或者enjarify之后重命名为thrift-protocol.jar放进libs目录)
+
+3. 启动服务端, 在手机安装客户端, 通过wifi连接
+
+4. Have fun
+
--- VirtualGraphicTablets-Server/bootstrap/src/main/java/sakura/kooi/VirtualGraphicTablets/server/bootstrap/Bootstrap.java
+++ VirtualGraphicTablets-Server/bootstrap/src/main/java/sakura/kooi/VirtualGraphicTablets/server/bootstrap/Bootstrap.java
@@ -4,10 +4,12 @@ import lombok.extern.slf4j.Slf4j;
 
 import javax.swing.*;
 import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 @Slf4j
 public class Bootstrap {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MalformedURLException {
         log.info("Checking classpath...");
         File libsDir = new File("libs");
         File coreJar = new File(libsDir, "vgt-core.jar");
@@ -15,12 +17,13 @@ public class Bootstrap {
             JOptionPane.showMessageDialog(null, "VirtualGraphicTablets Core not found\nPath: ./libs/vgt-core.jar", "Launch failed", JOptionPane.ERROR_MESSAGE);
             return;
         }
-        File protocolJar = new File(libsDir, "classes-dex2jar.jar");
+        File protocolJar = new File(libsDir, "thrift-protocol.jar");
         if (!protocolJar.exists()) {
-            JOptionPane.showMessageDialog(null, "VirtualTablet thrift protocol not found\nPath: ./libs/classes-dex2jar.jar", "Launch failed", JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(null, "VirtualTablet thrift protocol not found\nPath: ./libs/thrift-protocol.jar", "Launch failed", JOptionPane.ERROR_MESSAGE);
             return;
         }
 
+        OpenURLClassLoader coreLoader = new OpenURLClassLoader(new URL[] {coreJar.toURL()}, Bootstrap.class.getClassLoader());
         // TODO create a hybrid class loader
         // TODO first lookup core then VirtualTablets apk
         // aim: maven thrift artifact packed with core module instead of dex2jared thrift from apk
