6ddb986ff1f7a8ef1f862cc2c4a97e7e548f2c2e
sakurawa <root@sakurakooi.cyou>
2022-08-24 10:21:35 +0800

    [coding] implement invoke launch

--- VirtualGraphicTablets-Server/bootstrap/src/main/java/sakura/kooi/VirtualGraphicTablets/server/bootstrap/Bootstrap.java
+++ VirtualGraphicTablets-Server/bootstrap/src/main/java/sakura/kooi/VirtualGraphicTablets/server/bootstrap/Bootstrap.java
@@ -7,6 +7,7 @@ import javax.swing.*;
 import java.io.File;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.lang.reflect.Method;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -17,20 +18,28 @@ public class Bootstrap {
         File libsDir = new File("libs");
         File coreJar = new File(libsDir, "vgt-core.jar");
         if (!coreJar.exists()) {
+            log.c("vgt-core.jar not found in 'libs' directory.");
             JOptionPane.showMessageDialog(null, "VirtualGraphicTablets Core not found\nPath: ./libs/vgt-core.jar", "Launch failed", JOptionPane.ERROR_MESSAGE);
             return;
         }
         File protocolJar = new File(libsDir, "thrift-protocol.jar");
         if (!protocolJar.exists()) {
+            log.c("thrift-protocol.jar not found in 'libs' directory.");
             JOptionPane.showMessageDialog(null, "VirtualTablet thrift protocol not found\nPath: ./libs/thrift-protocol.jar", "Launch failed", JOptionPane.ERROR_MESSAGE);
             return;
         }
 
         try {
+            log.i("Creating core module classloader...");
             OpenURLClassLoader coreClassLoader = new OpenURLClassLoader(new URL[]{coreJar.toURI().toURL()}, Bootstrap.class.getClassLoader());
+            log.i("Creating protocol hybrid classloader...");
             LimitedClassLoader hybridClassLoader = new LimitedClassLoader(new URL[]{protocolJar.toURI().toURL()}, coreClassLoader);
 
+            log.i("Initializing core module...");
             Class<?> launcher = hybridClassLoader.findClass("sakura.kooi.VirtualGraphicTablets.server.core.Launch");
+            Method start = launcher.getMethod("start");
+            log.s("Invoking Launch.start() ...");
+            start.invoke(null);
         } catch (ReflectiveOperationException | MalformedURLException e) {
             StringWriter sw = new StringWriter();
             PrintWriter pw = new PrintWriter(sw);
