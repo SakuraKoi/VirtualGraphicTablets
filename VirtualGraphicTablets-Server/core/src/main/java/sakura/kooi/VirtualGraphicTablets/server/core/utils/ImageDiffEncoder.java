2af7675da3fc87222c4d376a93b01c23327713f5
sakurawa <root@sakurakooi.cyou>
2022-08-30 12:32:23 +0800

    [fix] improve encode performance

--- VirtualGraphicTablets-Server/core/src/main/java/sakura/kooi/VirtualGraphicTablets/server/core/utils/ImageDiffEncoder.java
+++ VirtualGraphicTablets-Server/core/src/main/java/sakura/kooi/VirtualGraphicTablets/server/core/utils/ImageDiffEncoder.java
@@ -1,69 +1,66 @@
 package sakura.kooi.VirtualGraphicTablets.server.core.utils;
 
-import io.netty.buffer.ByteBuf;
-import io.netty.buffer.ByteBufAllocator;
-
-import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
 
 public class ImageDiffEncoder {
-    private BufferedImage lastFrame;
-    private ByteBuf buffer;
+    private byte[] buffer;
 
     private int width, height;
 
     public ImageDiffEncoder(int tabletWidth, int tabletHeight) {
         this.width = tabletWidth;
         this.height = tabletHeight;
-        lastFrame = new BufferedImage(tabletWidth, tabletHeight, BufferedImage.TYPE_INT_ARGB);
-        buffer = ByteBufAllocator.DEFAULT.heapBuffer(width * height * 3);
+        buffer = new byte[width * height * 3];
     }
 
     public byte[] encode(BufferedImage currentFrame, boolean forceFullFrame) {
-        byte[] result = encodeDiff(lastFrame, currentFrame, forceFullFrame);
-        Graphics2D composite = lastFrame.createGraphics();
-        composite.clearRect(0, 0, width, height);
-        composite.drawImage(currentFrame, 0, 0, null);
-        composite.dispose();
-        return result;
+        int[] pixels = ((DataBufferInt) currentFrame.getRaster().getDataBuffer()).getData();
+        return encodeDiff(pixels, currentFrame.getWidth(), currentFrame.getHeight(), forceFullFrame);
     }
 
-    private byte[] encodeDiff(BufferedImage lastFrame, BufferedImage currentFrame, boolean forceFullFrame) {
-        int frameHeight = currentFrame.getHeight();
-        int frameWidth = currentFrame.getWidth();
+    private byte[] encodeDiff(int[] currentFrame, int frameWidth, int frameHeight, boolean forceFullFrame) {
         if (frameHeight > height)
             throw new IllegalArgumentException("too large frame height");
         if (frameWidth > width)
             throw new IllegalArgumentException("too large frame width");
 
-        buffer.resetWriterIndex();
+        int index = 0;
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
+                int r, g, b;
                 if (x < frameWidth && y < frameHeight) {
-                    int rgbCurrent = currentFrame.getRGB(x, y);
-                    if (lastFrame.getRGB(x, y) == rgbCurrent && !forceFullFrame) {
-                        writeRgb(buffer, 0xff, 0xff, 0xff);
+                    int rgbCurrent = currentFrame[y * frameWidth + x] & 0xFFFFFF;
+
+                    int start = (y * width + x) * 3;
+                    int rgbLast = ((buffer[start] & 0xFF) << 16) |
+                            ((buffer[start + 1] & 0xFF) << 8) |
+                            ((buffer[start + 2]) & 0xFF);
+
+                    if (rgbLast == rgbCurrent && !forceFullFrame) {
+                        r = g = b = 0xff;
                     } else {
-                        int r = (rgbCurrent >> 16) & 0xFF;
-                        int g = (rgbCurrent >> 8) & 0xFF;
-                        int b = (rgbCurrent) & 0xFF;
+                        r = (rgbCurrent >> 16) & 0xFF;
+                        g = (rgbCurrent >> 8) & 0xFF;
+                        b = (rgbCurrent) & 0xFF;
+
                         if (r == 0xff && g == 0xff && b == 0xff) {
                             r = g = b = 0xfe;
                         }
 
-                        writeRgb(buffer, r, g, b);
+                        buffer[start] = (byte) r;
+                        buffer[start + 1] = (byte) g;
+                        buffer[start + 2] = (byte) b;
                     }
                 } else { // 0x00, 0x00, 0x00 -> black
-                    writeRgb(buffer, 0x00, 0x00, 0x00);
+                    r = g = b = 0x00;
                 }
+
+                buffer[index++] = (byte) r;
+                buffer[index++] = (byte) g;
+                buffer[index++] = (byte) b;
             }
         }
-        return buffer.array();
-    }
-
-    private void writeRgb(ByteBuf buffer, int r, int g, int b) {
-        buffer.writeByte(r);
-        buffer.writeByte(g);
-        buffer.writeByte(b);
+        return buffer;
     }
 }
